82bd7c14ee740387084d828cb00696d874672b4e
diasraphael <diasraphael88@gmail.com>
2024-03-14 22:19:45 +0100

    changes for create user and login user

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from typing import List
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlite.database import engine
 from sqlite import models
 from sqlite.database import get_db
 from sqlalchemy.orm import Session
-from sqlite.schemas import UserRequest,UserResponse,LoginRequest,User
+from sqlite.schemas import UserRequest, UserResponse, LoginRequest, User
 from sqlite import db_user
 
 app = FastAPI()
@@ -21,50 +21,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-""" users = [
-    {
-        "user_id": "99f4df04-4c15-46e8-a8de-f053a782607f",
-        "username": "Dias",
-        "password": "$2b$12$FwDDzjskjI5tQlpF4gqc1um8ZJzWqHDvC2.BmBqA/kycAokC5ig.q",
-        "email": "diasraphael88@gmail.com",
-    }
-] """
-
-
-
-""" @app.post("/api/user")
-async def create_user(user: CreatePublicUser):
-    user_id = str(uuid.uuid4())
-    hashed_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
-    new_user = User(
-        user_id=user_id,
-        password=hashed_password.decode("utf-8"),
-        **user.dict(exclude={"user_id", "password"})
-    )
-    users.append(new_user.dict())
-    return new_user
-
-
-@app.post("/api/login")
-async def login_user(user: User):
-    for existing_user in users:
-        if existing_user["email"] == user.email and bcrypt.checkpw(
-            user.password.encode("utf-8"), existing_user["password"].encode("utf-8")
-        ):
-            return existing_user
-    return {"error": "Invalid username or password"} """
-
 
 @app.post("/api/user", response_model=UserResponse)
-async def create_user(request: UserRequest,db: Session=Depends(get_db)):
-    return db_user.create_user(db,request)
+async def create_user(request: UserRequest, db: Session = Depends(get_db)):
+    return db_user.create_user(db, request)
+
 
 @app.get("/api/users", response_model=List[UserResponse])
-async def get_all_users(db: Session=Depends(get_db)):
+async def get_all_users(db: Session = Depends(get_db)):
     return db_user.get_all_users(db)
 
+
 @app.post("/api/login", response_model=UserResponse)
-async def login_user(request: LoginRequest,db: Session=Depends(get_db)):
-    return db_user.login_user(db,request)
+async def login_user(request: LoginRequest, db: Session = Depends(get_db)):
+    return db_user.login_user(db, request)
+
 
 models.Base.metadata.create_all(engine)
--- sqlite/db_user.py
+++ sqlite/db_user.py
@@ -1,45 +1,58 @@
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import IntegrityError
-from .schemas import UserRequest,UserResponse
+from .schemas import UserRequest, UserResponse
 from .models import User
 from .hash import Hash
 from typing import List
 
 
-def create_user(db:Session, request:UserRequest)-> UserResponse:
-  try:
-    new_user = User(
-      username=request.username,
-      email=request.email,
-      password=Hash.bcrypt(request.password)
-      )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user) # this allows to give us an userId
-    return UserResponse(user_id=new_user.id, username=new_user.username, email=new_user.email)
-  except IntegrityError as e:
-    # Handle IntegrityError (UNIQUE constraint violation)
-    db.rollback()
-    print(f'Error creating user: {e}')
-  finally:
-    db.close()
+def create_user(db: Session, request: UserRequest) -> UserResponse:
+    try:
+        new_user = User(
+            username=request.username,
+            email=request.email,
+            password=Hash.bcrypt(request.password),
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)  # this allows to give us an userId
+        return UserResponse(
+            user_id=new_user.id, username=new_user.username, email=new_user.email
+        )
+    except IntegrityError as e:
+        # Handle IntegrityError (UNIQUE constraint violation)
+        db.rollback()
+        print(f"Error creating user: {e}")
+    finally:
+        db.close()
 
-def user_to_user_response(user:User):
-  return UserResponse(user_id=user.id, username=user.username, email=user.email)
 
-def get_all_users(db:Session)-> List[UserResponse]:
-  users = db.query(User).all()
-  return [user_to_user_response(user) for user in users]
+def user_to_user_response(user: User):
+    return UserResponse(user_id=user.id, username=user.username, email=user.email)
 
-def login_user(db:Session, request:UserRequest)-> UserResponse:
-  try:
-      user = db.query(User).filter(User.email == request.email and User.password == Hash.bcrypt(request.password)).first()
-      if user:
-            return UserResponse(user_id=user.id, username=user.username, email=user.email)
-      else:
-            return UserResponse(user_id=None, username=None, email=None)  
-  except Exception as e:
-    db.rollback()
-    print(f'Error creating user: {e}')
-  finally:
-    db.close()
+
+def get_all_users(db: Session) -> List[UserResponse]:
+    users = db.query(User).all()
+    return [user_to_user_response(user) for user in users]
+
+
+def login_user(db: Session, request: UserRequest) -> UserResponse:
+    try:
+        user = (
+            db.query(User)
+            .filter(
+                User.email == request.email
+                and User.password == Hash.bcrypt(request.password)
+            )
+            .first()
+        )
+        if user:
+            return UserResponse(
+                user_id=user.id, username=user.username, email=user.email
+            )
+        else:
+            return UserResponse(user_id=None, username=None, email=None)
+    except Exception as e:
+        print(f"Error creating user: {e}")
+    finally:
+        db.close()
