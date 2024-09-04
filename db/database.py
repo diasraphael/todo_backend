e2fee1a0cb746821153b096964131d1620c8e0a7
diasraphael <diasraphael88@gmail.com>
2024-09-04 18:06:50 +0200

    to use env file to pick values for local and prod

--- db/database.py
+++ db/database.py
@@ -1,8 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/todo"
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the database URL from environment variables
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
